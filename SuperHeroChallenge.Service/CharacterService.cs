d4ebd929202593061ada96a4c786a44f8d8b66ce
Gabriel <64979006+GabrielLibano@users.noreply.github.com>
2023-11-06 16:14:02 -0300

    Add files via upload

---
+++ SuperHeroChallenge.Service/CharacterService.cs
@@ -0,0 +1,59 @@
+﻿using SuperHero.Domain.Request;
+using SuperHero.Domain.Response;
+using SuperHero.Marvel.ACL.Interfaces;
+using SuperHero.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace SuperHero.Service
+{
+    public class CharacterService : ICharacterService
+    {
+        private readonly IMarvelAPI _marvelAPI;
+
+        public CharacterService(IMarvelAPI marvelAPI)
+        {
+            _marvelAPI = marvelAPI;
+        }
+        public CharacterResponse GetCharacter(CharacterRequest request)
+        {
+            try
+            {
+
+                if (request == null || request.Nome == string.Empty) throw new Exception("Requesição inválida!");
+
+                var marvelResult = _marvelAPI.GetCharacter(request.Nome);
+                CharacterResponse character = new CharacterResponse();
+                Thumbnail thumbnail = new Thumbnail();
+
+                character.Name = marvelResult.Name;
+                character.Desc = marvelResult.Description;
+                thumbnail.Path = marvelResult.Thumbnail.Path;
+                thumbnail.Extension = marvelResult.Thumbnail.Extension;
+                character.Thumbnail = thumbnail;
+                List<Event> eventList = new List<Event>();
+
+                foreach (var currentEvent in marvelResult.Events)
+                {
+                    Thumbnail thumbnailEvents = new Thumbnail();
+                    thumbnailEvents.Path = currentEvent.Thumbnail.Path;
+                    thumbnailEvents.Extension = currentEvent.Thumbnail.Extension;
+                    var jsonEvent = new Event
+                    {
+                        Title = currentEvent.Title,
+                        Desc = currentEvent.Description,
+                        Thumbnail = thumbnailEvents
+                    };
+                    eventList.Add(jsonEvent);
+                }
+                character.Events = eventList;
+                return character;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SuperHeroChallenge.Service/Interfaces/ICharacterService.cs
@@ -0,0 +1,10 @@
+﻿using SuperHero.Domain.Request;
+using SuperHero.Domain.Response;
+
+namespace SuperHero.Service.Interfaces
+{
+    public interface ICharacterService
+    {
+        CharacterResponse GetCharacter(CharacterRequest request);
+    }
+}
